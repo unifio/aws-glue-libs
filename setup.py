12891953d42f5d2d6c082c1571d14ee7ad38b7ce
Daniel <isaac@unif.io>
2019-04-10 15:58:01 -0700

    Creating setup.py to allow pip install
    * Adding a setup.py to permit the installation via pip to avoid errors on EMR nodes used to migrate away from glue dev endpoints.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awsglue',
+    author='awslabs',
+    version='0.0.1',
+    python_requires='>=2.7',
+    packages=find_packages(),
+    keywords=['apachespark', 'spark', 'pyspark']
+)
